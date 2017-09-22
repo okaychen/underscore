ebb60c5d0492ca66508f9f684807d89f0dcbbb05
okaychen <1440281580@qq.com>
2017-09-23 07:57:54 +0800

    add an internal function

--- underscore.js
+++ underscore.js
@@ -87,6 +87,24 @@
         return cb(value, context, Infinity);
     }
 
+    // 创建一个内部函数分配
+    var createAssigner = function (keysFunc, undefinedOnly) {
+        return function (obj) {
+            var length = arguments.length;
+            if (length < 2 || obj == null) return obj;
+            for (var index = 1; index < length; index++) {
+                var source = arguments[index],
+                    keys = keysFunc(source),
+                    l = keys.length;
+                for (var i = 0; i < l; i++) {
+                    var key = key[i];
+                    if (!undefined || obj[key] === void 0) obj[key] = source[key];
+                }
+            }
+            return obj;
+        }
+    }
+
 
 
 
