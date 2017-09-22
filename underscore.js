d9129484e8f7003fcfbdb2006e3ada59987659f6
okaychen <1440281580@qq.com>
2017-09-22 10:40:51 +0800

    add main function and brief description

--- underscore.js
+++ underscore.js
@@ -1,3 +1,9 @@
+//  okaychen (9月21号)
+//  underscore源码分析与学习
+//  希望可以通过这种方式学习underscore的代码思路
+//  选取的版本 underscore 1.8.3
+
+
 //整个函数在一个闭包中，避免全局变量污染。通过传入this，来改变函数的作用域
 (function () {
     //创建一个root对象，在浏览器中就是window，在服务器(如node)中就是'exports'
@@ -26,23 +32,62 @@
     var _ = function (obj) {
         if (obj instanceof _) return obj;
         if (!(this instanceof _)) return new _(obj);
-        this._wrapped = obj; 
+        this._wrapped = obj;
     }
 
     //针对不同的宿主对象，将underscore存放在不同的对象中
-    if (typeof exports !== 'undefined'){
-        if (typeof module !== 'undefined' && module.exports){
+    if (typeof exports !== 'undefined') {
+        if (typeof module !== 'undefined' && module.exports) {
             exports = module.exports = _;  //Node.js环境中/好熟悉的赶脚啊，哈哈
         }
         exports._ = _;
-    }else{
+    } else {
         root._ = _; //浏览器的宿主环境中将underscore挂载在window对象中
     }
 
     //现在的版本号
     _.VERSION = '1.8.3';
-    
-    
-    
-    
+
+    //核心函数1，对于参数小于等于4的情况分类处理：
+    var optimizeCb = function (func, context, argCount) {
+        if (context === void 0) return func;
+        switch (argCount == null ? 3 : argCount) {
+            case 1: return function (value) {
+                return func.call(context, value);
+            };
+            case 2: return function (value, other) {
+                return func.call(context, value, other);
+            };
+            case 3: return function (value, index, collection) {
+                return func.call(context, value, index, collection);
+            };
+            case 4: return function (accumulator, value, index, collection) {
+                return func.call(context, accumulator, value, index, collection);
+            };
+        }
+        return function () {
+            return func.apply(context, arguments);
+        }
+    }
+
+    /* 
+     * 
+     * 对参数进行判断，如果是函数则返回上面所说的回调函数；
+     * 如果是对象则返回一个能判断对象是否相等的函数；
+     * 默认返回一个获取对象属性的函数
+     * 
+     */
+    var cb = function (value, context, argCount) {
+        if (value == null) return _.identity;
+        if (_.inFunction(value)) return optimizeCb(value, context, argCount);
+        if (_.isObject(value)) return _.matcher(value);
+        return _.prototype(value);
+    }
+    _.iteratee = function (value, context) {
+        return cb(value, context, Infinity);
+    }
+
+
+
+
 }.call(this));
