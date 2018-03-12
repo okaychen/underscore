bd85560521f9fd62de71fabcf569c279a214931e
okaychen <1440281580@qq.com>
2018-03-12 18:07:59 +0800

    understand _.debounced function

--- underscore.js
+++ underscore.js
@@ -5,7 +5,7 @@
 
 
 //整个函数在一个闭包中，避免全局变量污染。通过传入this，来改变函数的作用域
-(function() {
+(function () {
     //创建一个root对象，在浏览器中就是window，在服务器(如node)中就是'exports'
     var root = this;
 
@@ -26,10 +26,10 @@
         navtiveCreate = Object.create;
 
     //裸函数
-    var Ctor = function() {};
+    var Ctor = function () {};
 
     //为下划线对象创建一个安全索引,（下划线在此定义）
-    var _ = function(obj) {
+    var _ = function (obj) {
         if (obj instanceof _) return obj;
         if (!(this instanceof _)) return new _(obj);
         this._wrapped = obj;
@@ -49,27 +49,27 @@
     _.VERSION = '1.8.3';
 
     //核心函数1，对于参数小于等于4的情况分类处理：
-    var optimizeCb = function(func, context, argCount) {
+    var optimizeCb = function (func, context, argCount) {
         if (context === void 0) return func;
         switch (argCount == null ? 3 : argCount) {
             case 1:
-                return function(value) {
+                return function (value) {
                     return func.call(context, value);
                 };
             case 2:
-                return function(value, other) {
+                return function (value, other) {
                     return func.call(context, value, other);
                 };
             case 3:
-                return function(value, index, collection) {
+                return function (value, index, collection) {
                     return func.call(context, value, index, collection);
                 };
             case 4:
-                return function(accumulator, value, index, collection) {
+                return function (accumulator, value, index, collection) {
                     return func.call(context, accumulator, value, index, collection);
                 };
         }
-        return function() {
+        return function () {
             return func.apply(context, arguments);
         }
     }
@@ -81,19 +81,19 @@
      * 默认返回一个获取对象属性的函数
      * 
      */
-    var cb = function(value, context, argCount) {
+    var cb = function (value, context, argCount) {
         if (value == null) return _.identity;
         if (_.inFunction(value)) return optimizeCb(value, context, argCount);
         if (_.isObject(value)) return _.matcher(value);
         return _.prototype(value);
     }
-    _.iteratee = function(value, context) {
+    _.iteratee = function (value, context) {
         return cb(value, context, Infinity);
     }
 
     // 创建一个内部函数分配
-    var createAssigner = function(keysFunc, undefinedOnly) {
-        return function(obj) {
+    var createAssigner = function (keysFunc, undefinedOnly) {
+        return function (obj) {
             var length = arguments.length;
             if (length < 2 || obj == null) return obj;
             for (var index = 1; index < length; index++) {
@@ -110,7 +110,7 @@
     }
 
     //用于创建从另一个对象继承新对象的内部函数
-    var baseCreate = function(prototype) {
+    var baseCreate = function (prototype) {
         if (!_.isObject(prototype)) return {};
         if (navtiveCreate) return navtiveCreate(prototype);
         Ctor.prototype = prototype;
@@ -118,8 +118,8 @@
         Ctor.prototype = null;
         return result;
     };
-    var property = function(key) {
-        return function(obj) {
+    var property = function (key) {
+        return function (obj) {
             return obj == null ? void 0 : obj[key];
         }
     }
@@ -127,13 +127,13 @@
     // 为收集方法做准备，应作为数组或对象的迭代
     var MAX_ARRAY_INDEX = Math.pow(2, 53) - 1;
     var getLength = property('length');
-    var isArrayLink = function(collection) {
+    var isArrayLink = function (collection) {
         var length = getLength(collection);
         return typeof length == 'number' && length >= 0 && length <= MAX_ARRAY_INDEX;
     }
 
     // _.each方法
-    _.each = _.forEach = function(obj, iteratee, context) {
+    _.each = _.forEach = function (obj, iteratee, context) {
         iteratee = optimizeCb(iteratee, context);
         var i, length;
         if (isArrayLink(obj)) {
@@ -150,3 +150,45 @@
     }
 
 }.call(this));
+
+// 去抖函数，传入的函数在wait时间之后（或者之前）执行，并且只会被执行一次。
+// 如果immediate传递为true，那么在函数被传递时就立即调用
+// 实现原理：涉及异步JavaScript，多次调用_.debounce返回的函数，会一次执行完，但是每次调用
+// 该函数又会清空上一次的TimeoutID，所以实际上只执行了最后一个setTimeout的内容
+_.debounce = function (func, wait, immediate) {
+    var timeout, result;
+
+    var later = function (context, args) {
+        timeout = null;
+        if (args) result = func.apply(context, args);
+    };
+
+    // 被返回的函数，该函数只会调用一次。
+    var debounced = restArgs(function (args) {
+        // 这行代码的作用是清除上一次的TimeoutID,
+        // 使得如果有多次调用该场景的函数时，只执行最后一次调用的延时
+        if (timeout) clearTimeout(timeout);
+        if (immediate) {
+            // 如果传递了immediate并且timeout为空，那么就立即调用func，否则不立即调用。
+            var callNow = !timeout;
+            //下面这行代码，later函数内部的func函数注定不会被执行，因为没有给later传递参数。
+            //它的作用是确保返回了一个timeout,并且保持到wait毫秒之后，才执行later，
+            //清空timeout。而清空timeout是在immediate为true时，callNow为true的条件。
+            //timeout = setTimeout(later, wait)的存在是既保证上升沿触发，
+            //又保证wait内最多触发一次的必要条件。
+            timeout = setTimeout(later, wait);
+            if (callNow) result = func.apply(this, args);
+        } else {
+            // 如果没有传递immediate，那就使用_.delay函数延时执行later。
+            timeout = _.delay(later, wait, this, args);
+        }
+        return result;
+    });
+
+    // 该函数用于取消当前去抖函数
+    debounced.cancel = function () {
+        clearTimeout(timeout);
+        timeout = null;
+    };
+    return debounced;
+}
